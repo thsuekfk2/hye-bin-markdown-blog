9ccb1931ca774154c4812ede64d46ee650170eda
thsuekfk2 <thsuekfk2@naver.com>
2023-07-23 16:51:13 +0900

    refactor : img -> next/image 리팩토링

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 export default async function Page() {
   return (
     <div>
-      <img src="./jump.gif" alt="" />
+      <Image
+        src="/jump.gif"
+        width={850}
+        height={300}
+        alt="Picture of the author"
+      />
       <h2>주니어 프론트엔드 개발자 이혜빈입니다.</h2>
       <ul>
         <li>☕ 고양이와 커피를 좋아합니다</li>
