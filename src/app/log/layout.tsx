af39d8a4cda6d8b5eb20aaa20974732a50ddb961
thsuekfk2 <thsuekfk2@naver.com>
2023-10-26 00:26:28 +0900

    style : log layout margin  추가

--- src/app/log/layout.tsx
+++ src/app/log/layout.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from "react";
 
 export default function Loglayout({ children }: PropsWithChildren) {
   return (
-    <div className="items-center justify-center w-full h-full">
+    <div className="items-center justify-center w-full h-full ml-3 mr-3">
       {children}
       <div className="flex justify-center fixed bottom-0 p-5 right-[50px] w-[100px] h-[100px] items-center">
         <Calendar />
