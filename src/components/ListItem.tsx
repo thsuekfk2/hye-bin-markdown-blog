c613312c207d149fe5eb67168bcd2de3653c14c2
thsuekfk2 <thsuekfk2@naver.com>
2023-11-30 18:17:04 +0900

    style : css 수정

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -5,7 +5,7 @@ export const ListItem = ({ date, title }: { date: string; title: string }) => {
   return (
     <Link
       href={`log/${date}`}
-      className="justify-between font-normal italic text-xs flex items-center w-[100%]  h-[20px] rounded-lg transition duration-300 hover:font-bold   p-5 break-words"
+      className="justify-between font-normal text-xs flex items-center w-[100%]  h-[20px] rounded-lg transition duration-300 hover:font-bold p-5 break-words"
     >
       <div className="hover:underline">{title}.</div>
       <div className="text-[#3c3c3c]">{date}</div>
