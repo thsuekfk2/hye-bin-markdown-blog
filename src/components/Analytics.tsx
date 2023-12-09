17c5b092dbb2a8ea32e456418f0a7e28cc2fed12
thsuekfk2 <thsuekfk2@naver.com>
2023-12-09 12:08:50 +0900

    feat : google-site-verification HTML태그 추가

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -7,6 +7,10 @@ export const Analytics = () => {
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
       ></Script>
+      <meta
+        name="google-site-verification"
+        content="iMPrP8ilvDDTleMZfYPnzYEFk9HmhiVInzKLOId0smg"
+      />
       <script
         dangerouslySetInnerHTML={{
           __html: `
