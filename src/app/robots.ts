fba1f09707e361ed557453627bb2126e46ae1183
thsuekfk2 <thsuekfk2@naver.com>
2025-06-02 13:16:22 +0900

    chore: sitemap 주소 변경

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.hyebin.info/sitemap.xml",
+    sitemap: "https://www.hyebin.me/sitemap.xml",
   };
 }
