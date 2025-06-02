ff8d6f461fc10954ed49152f52fe68255bde21ca
thsuekfk2 <thsuekfk2@naver.com>
2025-06-02 13:42:56 +0900

    style: hover underline 효과 제거

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -7,7 +7,7 @@ export const ListItem = ({ date, title }: { date: string; title: string }) => {
       href={`log/${date}`}
       className="flex h-[20px] w-[100%] items-center justify-between break-words rounded-lg p-5 text-xs font-normal transition duration-300 hover:font-bold"
     >
-      <div className="hover:underline">{title}.</div>
+      <div>{title}</div>
       <div className="text-[#3c3c3c]">{date}</div>
     </Link>
   );
