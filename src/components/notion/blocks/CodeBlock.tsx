3e506163d36b6e93125519a5ee6869f78ff2a3c1
thsuekfk2 <thsuekfk2@naver.com>
2025-10-21 09:22:47 +0900

    fix: 코드 블록이 중간에 잘리는 문제 해결

--- src/components/notion/blocks/CodeBlock.tsx
+++ src/components/notion/blocks/CodeBlock.tsx
@@ -6,7 +6,8 @@ interface CodeBlockProps {
 }
 
 export function CodeBlock({ block }: CodeBlockProps) {
-  const codeContent = block.code?.rich_text?.[0]?.plain_text || "";
+  const codeContent =
+    block.code?.rich_text?.map((text) => text.plain_text).join("") || "";
   const language = block.code?.language || "text";
 
   return <NotionCodeBlock code={codeContent} language={language} />;
